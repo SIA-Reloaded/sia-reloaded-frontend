73764eff022d09453fdbbebcea5e06941a9704d5
santosfernando99 <santosfernando99@outlook.com>
2020-09-26 12:45:22 -0500

    Initial Vue commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <navbar></navbar>
+  <article>
+    <router-view></router-view>
+  </article>
+</template>
+
+<script>
+import navbar from "./components/navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    navbar,
+  },
+};
+</script>
+
+<style>
+@font-face {
+  font-family: AncizarSansRegular;
+  src: url("./assets/ancizar-font/AncizarSans-Regular.otf");
+}
+@font-face {
+  font-family: AncizarSansLight;
+  src: url("./assets/ancizar-font/AncizarSans-Light.otf");
+}
+@font-face {
+  font-family: AncizarSansItalic;
+  src: url("./assets/ancizar-font/AncizarSans-Italic.otf");
+}
+body {
+  margin: 0;
+}
+#app {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 10vh 90vh;
+}
+navbar {
+  grid-row: 1/2;
+}
+article {
+  grid-row: 2/3;
+}
+</style>
