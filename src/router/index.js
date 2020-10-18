90c7b62ac67e0e40783186b8720d376534808bd6
Juan Guillermo Sierra <jgsierraa@unal.edu.co>
2020-10-18 16:06:28 -0500

    router changes, nothing serious boys

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,10 @@ import CourseComponent from '../components/course';
 
 const routes = [
     {
+        path: '/',
+        redirect: '/home'
+    },
+    {
         path: '/login',
         name: 'login',
         component: LoginComponent,
